053681acd7b3dab5c3720c5b0b88e587193f0b69
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-03-01 17:57:57 +0100

    Change handle quantity functions

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ProductDetails from "./components/ProductDetails";
 import Cart from "./components/Cart";
 import FilteredPage from "./components/FilteredPage";
 import { Routes, Route } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { products } from "./components/products/products";
 
 
@@ -13,36 +13,13 @@ function App() {
   const [cart, setCart] = useState([]);
   const [cartQuantity, setCartQuantity] = useState([]);
 
-  useEffect(() => {
-
-  }, [cart])
-
-
-  function handleClick(object) {
-    const index = cart.findIndex((item) => item.id === object.id);
-    if (index > -1) {
-      const newQuantity = cartQuantity.map((item, i) => {
-        if (i === index) {
-          return item + 1;
-        }
-        return item;
-      });
-      setCartQuantity(newQuantity);
-    }
-    else {
-      setCart(cart.concat(object));
-      setCartQuantity(cartQuantity.concat(1));
-    }
-  }
-
-
   return (
     <>
       <Header cartQuantity={cartQuantity} />
       <Routes>
         <Route path="/" element={<Page />} />
         <Route path="/cart" element={<Cart cart={cart} cartQuantity={cartQuantity} />} />
-        <Route path="/:id" element={<ProductDetails handleClick={handleClick} />} />
+        <Route path="/:id" element={<ProductDetails cart={cart} cartQuantity={cartQuantity} setCart={setCart} setCartQuantity={setCartQuantity} />} />
         <Route path="/board-games" element={<FilteredPage products={products.filter((item) => item.category === "board-game")} />} />
       </Routes>
       <Footer />
--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,8 +1,8 @@
 import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Modal from './Modal';
 
-function ProductDetails({ handleClick }) {
+function ProductDetails({ cart, setCart, cartQuantity, setCartQuantity }) {
     let { state } = useLocation();
     const [selectedImage, setSelectedImage] = useState(state.images[0]);
     const [zoom, setZoom] = useState(false);
@@ -22,6 +22,39 @@ function ProductDetails({ handleClick }) {
         setZoom(false);
     }
 
+    function changeQuantity(e) {
+        const quantityInput = document.querySelector("#quantity");
+        if (e.target.value === "plus") {
+            quantityInput.value = Number(quantityInput.value) + 1;
+        } else {
+            quantityInput.value = Number(quantityInput.value) - 1;
+        }
+    }
+
+    useEffect(() => {
+        const quantityInput = document.querySelector("#quantity");
+        const button = document.querySelector(".addToCart")
+
+        function handleClick() {
+            const index = cart.findIndex((item) => item.id === state.id);
+            if (index < 0) {
+                setCart(cart.concat(state));
+                setCartQuantity(cartQuantity.concat(Number(quantityInput.value)));
+            } else if (cartQuantity[index]) {
+                const newQuantity = cartQuantity.map((item, i) => {
+                    if (i === index) {
+                        return Number(quantityInput.value);
+                    } else {
+                        return item;
+                    }
+                });
+                setCartQuantity(newQuantity);
+            }
+        }
+        button.addEventListener("click", handleClick);
+
+    }, [cartQuantity])
+
     return (
         <div className="single-product">
             <div className="product-details">
@@ -36,7 +69,11 @@ function ProductDetails({ handleClick }) {
                 }
                 )}
             </div>
-            <button type="button" onClick={() => handleClick(state)}>Add to cart</button>
+            <label htmlFor="quantity">Quantity</label>
+            <input type="number" id="quantity" defaultValue="1" min="1" max={state.quantity} />
+            <button type="button" value="plus" onClick={changeQuantity}>+</button>
+            <button type="button" value="minus" onClick={changeQuantity}>-</button>
+            <button type="button" className="addToCart" >Add to cart</button>
             <Modal selectedImage={selectedImage} zoom={zoom} close={closeZoom} images={state.images} setSelectedImage={setSelectedImage} />
         </div>)
 
