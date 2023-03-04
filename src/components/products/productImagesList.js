33836d4eded9139f799b59ecfa8f840e35b83b08
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-03-04 11:52:39 +0100

    Add products and images

--- src/components/products/productImagesList.js
+++ src/components/products/productImagesList.js
@@ -1,9 +1,30 @@
 import root1 from "./images/root-gamebox.webp";
 import root2 from "./images/root-board.webp"
 import root3 from "./images/root-components.webp"
-import scythe1 from "./images/scythe-gamebox.png"
+import scythe1 from "./images/scythe-gamebox.webp"
 import scythe2 from "./images/scythe-board.jpg"
 import scythe3 from "./images/scythe-components.webp"
+import ttr1 from "./images/ttr.webp";
+import ttr2 from "./images/ttr2.webp";
+import marcoPolo1 from "./images/marco-polo-gamebox.webp"
+import marcoPolo2 from "./images/marco-polo-board.webp"
+import marcoPolo3 from "./images/marco-polo-components.webp"
+import wingspan1 from "./images/wingspan-gamebox.webp"
+import wingspan2 from "./images/wingspan-board.webp"
+import nimmt from "./images/6nimmt.jpg"
+import splendor1 from "./images/splendor-gamebox.webp"
+import splendor2 from "./images/splendor-board.webp"
+import pandemic1 from "./images/pandemic-gamebox.webp"
+import quantum1 from "./images/quantum-gamebox.webp"
+import quantum2 from "./images/quantum-board.webp"
+import quantum3 from "./images/quantum-components.jpg"
+import dAndD1 from "./images/d&d-cover.webp"
+import dAndD2 from "./images/d&d-components.webp"
+import carcassone1 from "./images/carcassone-gamebox.webp"
+import carcassone2 from "./images/carcassone-components.jpg"
+import pathfinder1 from "./images/pathfinder-cover.webp"
+import got1 from "./images/got-gamebox.webp"
+import got2 from "./images/got-components.jpg"
 
 export const productImages = [
     {
@@ -11,5 +32,39 @@ export const productImages = [
     },
     {
         scytheImages: [scythe1, scythe2, scythe3]
+    },
+    {
+        ttrImages: [ttr1, ttr2]
+    },
+    {
+        marcoPoloImages: [marcoPolo1, marcoPolo2, marcoPolo3]
+    },
+    {
+        wingspanImages: [wingspan1, wingspan2]
+    },
+    {
+        nimmtImages: [nimmt]
+    },
+    {
+        splendorImages: [splendor1, splendor2]
+    },
+    {
+        pandemicImages: [pandemic1]
+    },
+    {
+        quantumImages: [quantum1, quantum2, quantum3]
+    },
+    {
+        dAndDImages: [dAndD1, dAndD2]
+    },
+    {
+        carcassoneImages: [carcassone1, carcassone2]
+    },
+    {
+        pathfinderImages: [pathfinder1]
+    },
+    {
+        gotImages: [got1, got2]
     }
+
 ]
