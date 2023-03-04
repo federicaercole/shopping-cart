b9d290e5d3ac266d67bd0b134f972c7719afdf69
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-03-04 12:48:31 +0100

    Add basename to router

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.querySelector('#app'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
