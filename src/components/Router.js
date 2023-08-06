2003292591723f3a5bc2abc7731ae640a40cc262
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-08-06 12:26:30 +0200

    Refactor router in a new file

---
+++ src/components/Router.js
@@ -0,0 +1,39 @@
+import App from '../App';
+import Homepage from "./Homepage";
+import ProductDetails from "./ProductDetails";
+import Cart from "./Cart";
+import FilteredPage from "./FilteredPage";
+import ErrorPage from './ErrorPage';
+import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from "react-router-dom";
+import { loadProduct, loadHomepageProducts, loadCategoryProducts, loadSearchResults } from './utils/api';
+
+const Router = () => {
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route element={<App />} handle={{ crumb: () => "Home" }} errorElement={<ErrorPage />}>
+                <Route path="/" index element={<Homepage />} loader={loadHomepageProducts} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/search" element={<FilteredPage />} loader={loadSearchResults} />
+
+                <Route path="/board-games" handle={{ crumb: () => "Board Games" }}>
+                    <Route index element={<FilteredPage key="board-games" title="All Board Games" />} loader={loadCategoryProducts} />
+                    <Route path=":productId" element={<ProductDetails />} loader={loadProduct} handle={{ crumb: (data) => data.product.name }} />
+                </Route>
+
+                <Route path="/card-games" handle={{ crumb: () => "Card Games" }}>
+                    <Route index element={<FilteredPage key="card-games" title="All Card Games" />} loader={loadCategoryProducts} />
+                    <Route path=":productId" element={<ProductDetails />} loader={loadProduct} handle={{ crumb: (data) => data.product.name }} />
+                </Route>
+
+                <Route path="/rpgs" handle={{ crumb: () => "RPGs" }}>
+                    <Route index element={<FilteredPage key="rpgs" title="All RolePlaying Games" />} loader={loadCategoryProducts} />
+                    <Route path=":productId" element={<ProductDetails />} loader={loadProduct} handle={{ crumb: (data) => data.product.name }} />
+                </Route>
+                <Route path="*" element={<ErrorPage />} loader={loadCategoryProducts} />
+            </Route >
+        ));
+
+    return <RouterProvider router={router} />;
+}
+
+export default Router;
--- src/index.js
+++ src/index.js
@@ -2,46 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import './fonts/jost.woff2';
-import App from './App';
-import Homepage from "./components/Homepage";
-import ProductDetails from "./components/ProductDetails";
-import Cart from "./components/Cart";
-import FilteredPage from "./components/FilteredPage";
-import ErrorPage from './components/ErrorPage';
-import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements } from "react-router-dom";
 import { CartContextProvider } from './components/CartContext';
-import { loadProduct, loadHomepageProducts, loadCategoryProducts, loadSearchResults } from './components/utils/api';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route element={<App />} handle={{ crumb: () => "Home" }} errorElement={<ErrorPage />}>
-      <Route path="/" index element={<Homepage />} loader={loadHomepageProducts} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/search" element={<FilteredPage />} loader={loadSearchResults} />
-
-      <Route path="/board-games" handle={{ crumb: () => "Board Games" }}>
-        <Route index element={<FilteredPage key="board-games" title="All Board Games" />} loader={loadCategoryProducts} />
-        <Route path=":productId" element={<ProductDetails />} loader={loadProduct} handle={{ crumb: (data) => data.product.name }} />
-      </Route>
-
-      <Route path="/card-games" handle={{ crumb: () => "Card Games" }}>
-        <Route index element={<FilteredPage key="card-games" title="All Card Games" />} loader={loadCategoryProducts} />
-        <Route path=":productId" element={<ProductDetails />} loader={loadProduct} handle={{ crumb: (data) => data.product.name }} />
-      </Route>
-
-      <Route path="/rpgs" handle={{ crumb: () => "RPGs" }}>
-        <Route index element={<FilteredPage key="rpgs" title="All RolePlaying Games" />} loader={loadCategoryProducts} />
-        <Route path=":productId" element={<ProductDetails />} loader={loadProduct} handle={{ crumb: (data) => data.product.name }} />
-      </Route>
-      <Route path="*" element={<ErrorPage />} loader={loadCategoryProducts} />
-    </Route >
-  ));
+import Router from './components/Router';
 
 const root = ReactDOM.createRoot(document.querySelector('#app'));
 root.render(
   <React.StrictMode>
     <CartContextProvider>
-      <RouterProvider router={router} />
+      <Router />
     </CartContextProvider>
   </React.StrictMode>
 );
