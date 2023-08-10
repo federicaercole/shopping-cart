64b3f7169b0827689dd8b78aed7caf2b1ea9ef0d
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-08-10 10:16:38 +0200

    Search form validation and fix some styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,49 @@
 import { searchIcon } from "./icons";
 import Button from "./Button";
+import { useRef, useState, useEffect } from "react";
 import { Form } from "react-router-dom";
+import ErrorMessage from "./ErrorMessage";
+import { useLocation } from "react-router-dom";
 
 function SearchBar({ query, setQuery, setSubmittedInput }) {
+    const inputRef = useRef(null);
+    const [message, setMessage] = useState("");
+    const location = useLocation();
 
-    return <Form id="search-form" method="get" role="search" action="/search">
-        <label htmlFor="search" className="visually-hidden">Search game</label>
-        <input key="input" type="text" id="search" name="s" onChange={(e) => setQuery(e.target.value)} />
-        <Button type="submit" handle={() => setSubmittedInput(query)}>{searchIcon}<span className="visually-hidden">Search</span></Button>
-    </Form>
+    function handleSubmit(e) {
+        const value = inputRef.current.value;
+
+        if (!value) {
+            e.preventDefault();
+            setMessage("Please write a keyword to start a search");
+        } else {
+            setSubmittedInput(query);
+        }
+    }
+
+    function handleChange(e) {
+        const value = inputRef.current.value
+
+        if (value) {
+            setQuery(e.target.value);
+            setMessage("");
+        }
+    }
+
+    useEffect(() => {
+        setMessage("");
+    }, [location])
+
+    return (<>
+        <Form id="search-form" method="get" role="search" action="/search" >
+            <div>
+                <label htmlFor="search" className="visually-hidden">Search game</label>
+                <input key="input" type="text" id="search" name="s" ref={inputRef} onChange={(e) => handleChange(e)} aria-describedby="error-search" />
+                <Button type="submit" handle={(e) => handleSubmit(e)}>{searchIcon}<span className="visually-hidden">Search</span></Button>
+            </div>
+            <ErrorMessage message={message} id="search" />
+        </Form>
+    </>)
 }
 
 export default SearchBar;
